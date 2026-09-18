65ae58012610904c9b25afb349f00234f5abc0f9
Polar-404-exe <antoniomigliazza10@gmail.com>
2026-09-18 23:57:38 +0200

    .

--- title_screen.gd
+++ title_screen.gd
@@ -15,5 +15,4 @@ func _on_quit_pressed() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_scene.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
